43c85ec4e4d3c8412b329b6bfe5f0d6bb4db07cc
Nathaniel Schweinberg <natanielks@gmail.com>
2017-09-13 16:37:06 -0700

    force use of /usr/bin/base64 for mac users with gnuutils installed

--- generate_token.sh
+++ generate_token.sh
@@ -2,4 +2,4 @@
 hostport=$1
 secret=$2
 signature=$(echo -n "${hostport}${secret}" | shasum -a 256 | cut -d ' ' -f 1)
-echo $(echo -n "$hostport $signature" | base64)
+echo $(echo -n "$hostport $signature" | /usr/bin/base64)
